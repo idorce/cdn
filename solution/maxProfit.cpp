dd31ec41b6856f9073ac4e1b8a29e48636d9c930
idorce <idorce@outlook.com>
2018-11-26 22:43:46 +0800

    121. Best Time to Buy and Sell Stock

--- solution/maxProfit.cpp
+++ solution/maxProfit.cpp
@@ -4,4 +4,16 @@
 
 #include "../solution.h"
 
-int Solution::maxProfit(vector<int> &prices) {}
+int Solution::maxProfit(vector<int> &prices) {
+    if (prices.size() <= 1) return 0;
+
+    int minPrice = prices[0], maxPro = 0;
+
+    for (auto p = prices.begin() + 1;p != prices.end();++p) {
+        int profit = *p - minPrice;
+        if (profit < 0) minPrice = *p;
+        else if (profit > maxPro) maxPro = profit;
+    }
+
+    return maxPro;
+}
