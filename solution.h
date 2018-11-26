614d564bbd3dc33580b7c26d63661695c2b15dff
idorce <idorce@foxmail.com>
2018-11-26 19:56:54 +0800

    First commit

---
+++ solution.h
@@ -0,0 +1,22 @@
+//
+// Created by ID J on 2018/11/26.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices);
+};
+
+
+#endif //LEETCODE_SOLUTION_H
---
+++ solution/maxProfit.cpp
@@ -0,0 +1,7 @@
+//
+// Created by ID J on 2018/11/26.
+//
+
+#include "../solution.h"
+
+int Solution::maxProfit(vector<int> &prices) {}
