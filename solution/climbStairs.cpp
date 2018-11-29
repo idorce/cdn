fe1dac0c57ecec455814b60903e415399cd292cc
idorce <idorce@outlook.com>
2018-11-29 23:57:38 +0800

    70. Climbing Stairs

--- solution/climbStairs.cpp
+++ solution/climbStairs.cpp
@@ -1,8 +1,6 @@
 int climbStairs(int n) {
-    if (n <= 1) return 1;
-    
     int dp__ = 0, dp_ = 1, dp;
-    while (--n) {
+    while (n > 0 && --n) {
         dp = dp__ + dp_;
         dp__ = dp_;
         dp_ = dp;
